5ef49a5d8e4b72e4ec8e60007ff176bbda7038b9
soufian <soufian.salim@gmail.com>
2012-09-24 15:15:59 +0200

    scripts bin/

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -1,8 +1,12 @@
 <?php
-// Define path to application directory
+
+set_time_limit(0);
+
+defined('ROOT_PATH')
+    || define('ROOT_PATH', dirname(__FILE__) . '/../');
+
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', dirname(__FILE__) . '/../app/');
 
-// Define path to root directory
-defined('ROOT_PATH')
-    || define('ROOT_PATH', dirname(__FILE__) . '/../');
+require_once APPLICATION_PATH . '/bootstrap.php.cache';
+require_once APPLICATION_PATH . '/AppKernel.php';
