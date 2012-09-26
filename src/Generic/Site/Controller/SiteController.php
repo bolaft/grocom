b647fcb4035dffb77342b8596044d840a8019c42
Soufian <soufian@tux.(none)>
2012-09-26 13:50:28 +0200

    Gestion des traductions

--- src/Generic/Site/Controller/SiteController.php
+++ src/Generic/Site/Controller/SiteController.php
@@ -3,11 +3,17 @@
 namespace Generic\Site\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class SiteController extends Controller
 {
     public function indexAction()
     {
         return $this->render('GenericSite::index.html.twig');
+    }
+
+    public function pageNotFoundAction()
+    {
+        return $this->render('GenericSite::page_not_found.html.twig');
     }
 }
