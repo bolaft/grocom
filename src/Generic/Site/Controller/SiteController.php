b9a14cdea4a0d0195c267e9fbac9dbafc7f8790d
s-salim <soufian.salim@gmail.com>
2012-09-26 22:08:04 +0200

    modifs vues

--- src/Generic/Site/Controller/SiteController.php
+++ src/Generic/Site/Controller/SiteController.php
@@ -3,7 +3,6 @@
 namespace Generic\Site\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class SiteController extends Controller
 {
